19b5987f87650cba7df6ce7b11e32a85b9b96eb6
benbasty <benbasty@yahoo.fr>
2023-01-16 15:41:32 +0800

    modified home component

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import DownloadAndContact from "./DownloadAndContact";
-import ME from "../src/assets/me.JPG";
+import ME from "../src/assets/mxe.png";
 import HeaderSocialIcons from "./HeaderSocialIcons";
 import '../src/header.css';
 
@@ -13,13 +13,19 @@ const Header = () => {
                 <h4 className="text-primary">Front-End Developer</h4>
                 <DownloadAndContact />
                 <div className="me">
-                    <img src={ME} alt=""/>
+                    <div className="image">
+                        <img src={ME} alt=""/>
+                    </div>
+                    
                 </div>
-                
                 <a href="#about" className="scroll__down">About Me</a>
-                
                 <HeaderSocialIcons />
+                
             </div>
+            <div className="social-media-container">
+                <h1>HI</h1>
+            </div>
+            
             
         </header>
     )
