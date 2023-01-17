a5641fd3248690dc43d45adbcd3bdc1fd263c6f4
benbasty <benbasty@yahoo.fr>
2023-01-18 07:08:06 +0800

    fixed footer component and about component display

--- src/About.js
+++ src/About.js
@@ -8,49 +8,37 @@ import '../src/about.css';
 
 const About = () => {
     return(
-        <section id="about">
+        <div id="about">
             {/* <h5>Biography</h5>
             <h1>About Me</h1> */}
-            <div className="about__container container">
-
-                {/* <div className="about__me">
-                    <div className="about__me-image">
-                        <img src={ME} alt="about me"/>
-                    </div>
-                </div>  */}
-
-
-                <div className="about__content">
-                    <div className="about__cards">
-                        <article className="about__card">
-                            <FiUsers className="about__icon"/>
-                            <h5>Biography</h5>
-                            <small>I'm Ben and I'm passionate about developping beautiful front-end interfaces.</small>
-
-                        </article>
-                        <article className="about__card">
-                            <FaAward className="about__icon"/>
-                            <h5>Experience</h5>
-                            <small>1 Year</small>
-                        </article>
-                        <article className="about__card">
-                            <VscFolderLibrary className="about__icon"/>
-                            <h5>Projects</h5>
-                            <small>5+ Projects</small>
-                        </article>
-                    </div>
-                    {/* <p>Vitae suscipit tellus mauris a diam 
-                        maecenas sed enim. Laoreet id donec 
-                        ultrices tincidunt arcu non sodales. 
-                        Mauris commodo quis imperdiet massa 
-                        tincidunt nunc pulvinar sapien et. 
-                        Non pulvinar neque laoreet suspendisse 
-                        interdum consectetur.
-                    </p> */}
-                    {/* <a href="#contact" className="btn btn-primary">Let's Talk</a> */}
+            <div className="container about__container">
+                <div className="about__card">
+                    <article>
+                        <FiUsers className="about__icon"/>
+                        <h5>Biography</h5>
+                        <small>I'm Ben and I'm passionate about developping beautiful front-end interfaces.</small>
+                    </article>
                 </div>
+                <div className="about__card">
+                    <article>
+                        <FaAward className="about__icon"/>
+                        <h5>Experience</h5>
+                        <small>1 Year</small>
+                    </article>
+                </div>
+                <div className="about__card">
+                    <article>
+                        <VscFolderLibrary className="about__icon"/>
+                        <h5>Projects</h5>
+                        <small>5+ Projects</small>
+                    </article>
+                </div>
+                        
+                        
+                        
+                
             </div>
-        </section>
+        </div>
     )
 }
 export default About;
